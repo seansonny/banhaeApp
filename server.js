8bf01be43c8fd233ee91f2dc3788b644516765db
DongHyeokJeong <ddkkd1@naver.com>
2017-05-25 14:04:13 +0900

    basic setting

--- server.js
+++ server.js
@@ -2,22 +2,29 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var ingredientRouter = require('./router/ingredientRouter');
 var morgan = require('morgan');
+var bodyParser = require('body-parser');
+
+//라우터 include
+/*var ingredientRouter = require('./ingredientRouter');*/
+var brandRouter = require('./route/brand/brand.router');
+
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false}))
 
-app.use(ingredientRouter);
+//라우터 동작
+// app.use(ingredientRouter);
+app.use('/brands', brandRouter);
 
-app.use(function(err, req, res, next) {
-    console.log(err);
+app.use(function(req, res, next) {
     res.sendStatus(404);
 });
 
 app.use(function (err, req, res, next) {
-    console.log(err);
     res.status(500).send({mag: err.message});
 });
 
-app.listen(3330, function(){
-    console.log('Server is listening 3330');
+app.listen(3333, function(){
+    console.log('Server is listening 3333');
 });
