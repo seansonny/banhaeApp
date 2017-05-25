836795955e7d80e7f6cbc7c6279af8879492db68
sswpro10 <sswpro@gmail.com>
2017-05-25 10:49:51 +0900

    server

---
+++ server.js
@@ -0,0 +1,21 @@
+
+var express = require('express');
+//var bodyParser = require('body-parser');
+var ingredientRouter = require('./ingredientRouter');
+
+var app = express();
+//app.use(bodyParser.urlencoded({ extended: false}))
+
+app.use(ingredientRouter);
+
+app.use(function(req, res, next) {
+    res.sendStatus(404);
+});
+
+app.use(function (err, req, res, next) {
+    res.status(500).send({mag: err.message});
+});
+
+app.listen(3333, function(){
+    console.log('Server is listening 3333');
+})
