816f8e7bbf6a2feaee7f2de1570bed88e228421f
DongHyeokJeong <ddkkd1@naver.com>
2017-05-26 23:35:32 +0900

    정리

--- server.js
+++ server.js
@@ -1,20 +1,17 @@
 var express = require('express');
-var bodyParser = require('body-parser');
-var ingredientRouter = require('./router/ingredientRouter');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 
 //라우터 include
-/*var ingredientRouter = require('./ingredientRouter');*/
+var ingredientRouter = require('./router/ingredientRouter');
 var brandRouter = require('./route/brand/brand.router');
 
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ extended: false}))
 
 //라우터 동작
-// app.use(ingredientRouter);
+app.use(ingredientRouter);
 app.use('/brands', brandRouter);
 
 app.use(function(req, res, next) {
