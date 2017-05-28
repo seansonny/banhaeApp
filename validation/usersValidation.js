794aea3db56c50c9e57e27ba5edb85901191a0ca
sswpro10 <sswpro@gmail.com>
2017-05-28 12:03:12 +0900

    export modules

--- validation/usersValidation.js
+++ validation/usersValidation.js
@@ -6,15 +6,22 @@ class Validation{
 Validation.userInfo = function(req){
 
     return new Promise((resolve, reject)=> {
+
         try{
             const user_email = req.body.email;
             const nickname = req.body.nickname;
             const randomInt = Math.round(Math.random()*100000000);
-            const pw = req.body.pw + randomInt;
-            const hash = crypto.createHash('sha256').update(pw).digest('base64');
-            const gender = req.body.gender;
-            const birth = req.body.birth;
+            var pw = req.body.pw;
+
+            var gender = req.body.gender;
+            var birth = req.body.birth;
             const token = "token010203040"; //토큰 로직 필요
+
+            if (!user_email||!pw||!gender||!birth){
+                reject("필수 정보 누락 (이메일, 비번, 생년월일, 성별)");
+            }
+            pw += randomInt;
+            const hash = crypto.createHash('sha256').update(pw).digest('base64');
 
             var user_info = {
                 email : user_email,
