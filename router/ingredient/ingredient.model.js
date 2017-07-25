5e5be346f0e00682a80e6cc378a331be38c0ed7b
sswpro10 <sswpro@gmail.com>
2017-07-25 15:01:53 +0900

    회원가입 수정

--- router/ingredient/ingredient.model.js
+++ router/ingredient/ingredient.model.js
@@ -14,10 +14,10 @@ Ingredient.getIngredientDetail = function(ingredientId, sendCb) {
         //     'warning, is_warning FROM ingredient ' +
         //     'WHERE ingredient_id = '
         // + ingredientId; //sql injection 취약
-        var sql = 'SELECT l1, l2, role, role_num, ' +
+        var sql = 'SELECT L1, L2, role, role_num, ' +
             'description, allergy, allergy_num, ' +
             'warning, is_warning FROM ingredient ' +
-            'WHERE ingredient_id = ?'
+            'WHERE ingredient_id = ?';
 
         conn.query(sql, ingredientId, function(err, results){
             if ( err ) {
