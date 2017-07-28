ac9c1b1faafaa1062fad9feb2377abffc1db1818
sswpro10 <sswpro@gmail.com>
2017-07-28 21:35:16 +0900

    재료 상세

--- router/ingredient/ingredient.model.js
+++ router/ingredient/ingredient.model.js
@@ -14,7 +14,7 @@ Ingredient.getIngredientDetail = function(ingredientId, sendCb) {
         //     'warning, is_warning FROM ingredient ' +
         //     'WHERE ingredient_id = '
         // + ingredientId; //sql injection 취약
-        var sql = 'SELECT L1, L2, role, role_num, ' +
+        var sql = 'SELECT l1, l2, role, role_num, ' +
             'description, allergy, allergy_num, ' +
             'warning, is_warning FROM ingredient ' +
             'WHERE role_num = ?'
