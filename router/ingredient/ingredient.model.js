725c6f474be15dfa3ace6ef0e5f4c454f65732be
sswpro10 <sswpro@gmail.com>
2017-07-28 21:38:13 +0900

    재료 상세

--- router/ingredient/ingredient.model.js
+++ router/ingredient/ingredient.model.js
@@ -17,7 +17,7 @@ Ingredient.getIngredientDetail = function(ingredientId, sendCb) {
         var sql = 'SELECT l1, l2, role, role_num, ' +
             'description, allergy, allergy_num, ' +
             'warning, is_warning FROM ingredient ' +
-            'WHERE role_num = ?'
+            'WHERE id = ?'
         conn.query(sql, ingredientId, function(err, results){
             if ( err ) {
                 conn.release();
